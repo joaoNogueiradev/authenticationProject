338edb530a70cec61e82e832e76217659960a8ee
João Luiz Lima Nogueira <devjoaoluiz@gmail.com>
2024-03-11 02:26:11 -0300

    setting dotenv, env variables and app specificities, nodemailer settings

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -1,34 +1,74 @@
-const otpForm = document.querySelector('.otpForm')
+const otpForm = document.querySelector('.otpForm');
+const notification = document.querySelector('.notification')
+const otpField = document.getElementById('otpField')
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let otp = 0
 
 otpForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('clique')
+    notification.innerHTML = `
+    <strong>Loading</strong>
+    <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+    `
+
+    let token = Math.floor(Math.random() * 9000) + 1000;
+    
+    let emailData = {
+        name: name.value,
+        email: email.value,
+        subject: `OTP email`,
+        message: `Your OTP number is ${token}`
+    }
+    otp = token
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success'){
+            notification.innerHTML = '';
+            alert('Email sent!');
+            name.value = '',
+            email.value = ''
+        } else {
+            notification.innerHTML = '';
+            alert('Something went wrong, your Email was not Sent!')
+        }
+    }
+
+    xhr.send(JSON.stringify(emailData));
+
+    otpField.classList.remove('hide')
+
+    const firstInput = document.getElementById('otpNum1');
+    firstInput.focus();
+    
 });
 
 
-// const nodemailer = require('nodemailer');
-// const generatedToken = require('./script.js')
+submitButton.onclick = (e) => {
+    e.preventDefault();
 
-// const transport = nodemailer.createTransport({
-//     host:'smtp.gmail.com',
-//     port:465,
-//     secure:true,
-//     auth:{
-//             user:'tjs202624@gmail.com',
-//             pass:'wpdw mtjx yhap xzxq'
-//     }
-// });
+    let fieldNumbers = Number(otpNum1.value + otpNum2.value + otpNum3.value + otpNum4.value);
+    if(otp !== fieldNumbers){
+        alert('Authentication Failed!')
+    } else {
+        alert('Authentication Suceeded!')
+    }
+};
 
-// transport.sendMail({
-//     from:'João Nogueira <tjs202624@gmail.com>',
-//     to: `${destinationEmail.value}`,
-//     subject: `Token de ${emailDestinationName.value}`,
-//     html:`
-//         <h1>Olá, ${destinationEmail.value}</h1>
-//         <h2>Seu token de autenticação é: ${generatedToken}</h2>
-//     `,
-//     text: `Olá, seu token de autenticação é o: ${generatedToken}`
-// })
-// .then(() => console.log('Email enviado com sucesso!'))
-// .catch((err) => console.log('erro ao enviar email: ', err));
+const otpInputs = document.querySelectorAll('.otpNum');
+
+otpInputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+        if (input.value.length >= input.maxLength) {
+            // Move o foco para o próximo campo de entrada, se existir
+            if (index < otpInputs.length - 1) {
+                otpInputs[index + 1].focus();
+            }
+        }
+    });
+});
 
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,49 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+
 const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/public/otpForm.html')
+})
+
+app.post('/', (req,res) => {
+    console.log(req.body)
+
+    const transporter = nodemailer.createTransport({
+        host:'smtp.gmail.com',
+        port:465,
+        secure:true,
+        auth:{
+                user:process.env.EMAIL_ADM,
+                pass:process.env.EMAIL_PASSWORD
+        }
+    })
+
+    const mailOptions = {
+        from:process.env.EMAIL_ADM,
+        to:req.body.email,
+        subject:`${req.body.subject} for ${req.body.name}`,
+        text:req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error,info) => {
+        if(error){
+            console.log(error);
+            res.send('Error')
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
 })
 
 app.listen(PORT, () => {
